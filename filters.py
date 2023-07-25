df399db69e4f12c4ea06a5fa5f15f22a42b3379a
Alexandre Chataignon <alexandre.git@xouillet.info>
2023-07-25 02:59:16 +0200

    Handle resized pano

--- filters.py
+++ filters.py
@@ -1,15 +1,29 @@
 #! /usr/bin/python
 
 import datetime
-import jinja2
+import json
 
 
 def date_reformat(value, format_to):
     return datetime.datetime.strptime(value, "%Y-%m-%d").strftime(format_to)
 
 
-def is_pano(value):
-    try:
-        return value['xmpmeta']['RDF']['Description']['UsePanoramaViewer'] == "True"
-    except:
-        return False
+def panodata(media):
+    pano_xmp = media.xmp["xmpmeta"]["RDF"]["Description"]
+    if "UsePanoramaViewer" not in pano_xmp or pano_xmp["UsePanoramaViewer"] != "True":
+        return "null"
+
+    width_ratio = media.size["width"] / media.input_size["width"]
+    height_ratio = media.size["height"] / media.input_size["height"]
+
+    return json.dumps(
+        {
+            "fullWidth": int(pano_xmp["FullPanoWidthPixels"]) * width_ratio,
+            "fullHeight": int(pano_xmp["FullPanoHeightPixels"]) * height_ratio,
+            "croppedWidth": int(pano_xmp["CroppedAreaImageWidthPixels"]) * width_ratio,
+            "croppedHeight": int(pano_xmp["CroppedAreaImageHeightPixels"])
+            * height_ratio,
+            "croppedX": int(pano_xmp["CroppedAreaLeftPixels"]) * width_ratio,
+            "croppedY": int(pano_xmp["CroppedAreaTopPixels"]) * height_ratio,
+        }
+    )
